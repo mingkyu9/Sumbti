99ae3af82fd254ebcbe89446d49074ec0b6ae02a
PADO\saemi <sammyimm985@gmail.com>
2023-09-30 17:38:45 +0900

    로그인, 회원가입, 메인페이지, 하트 수정

--- src/main/java/app/sum/bti/couple/controller/CoupleController.java
+++ src/main/java/app/sum/bti/couple/controller/CoupleController.java
@@ -3,6 +3,7 @@ package app.sum.bti.couple.controller;
 import app.sum.bti.couple.service.CoupleService;
 import app.sum.bti.couple.vo.CoupleVO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,34 +12,46 @@ import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class CoupleController {
-
+    
+    // coupleZoneList 화면
     private final CoupleService coupleService;
-    @GetMapping("/zone")
-    public ModelAndView getCoupleList(){
+    @GetMapping("/coupleZoneList") // 회원가입 후 세션에서 아이디 가져오기
+    public ModelAndView getCoupleList(@RequestParam(value = "userId", defaultValue = "test1") String userId){
         ModelAndView view  = new ModelAndView();
-
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("userId",userId);
         try {
-            List<CoupleVO.CoupleList> coupleList = coupleService.getCoupleList();
+            List<CoupleVO.CoupleList> coupleList = coupleService.getCoupleList(params);
+            // 반복 돌리지 않고 스트림으로 처리?
+            coupleList = coupleList.stream().map( obj-> {
+                String mbti = obj.getUserMbti();
+                String img = "/img/profileIcon/" + (obj.getUserGender().equals("남자") ? "m-"+mbti+".png" :"f-"+mbti+".png");
+                obj.setImages(img);
+                return obj;
+            }).toList();
             view.addObject("coupleList",coupleList);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        view.setViewName("views/zonezone");
+        view.setViewName("views/coupleZone/coupleZoneList");
 
         return view;
     }
 
-
-    @GetMapping("/zone2")
-    public ModelAndView getCoupleListPick(){
+    // coupleZoneSelectedList 화면
+    @GetMapping("/coupleZoneSelectedList")
+    public ModelAndView getCoupleListPick(@RequestParam(value = "userId", defaultValue = "test1") String userId){
         ModelAndView view  = new ModelAndView();
-
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("userId",userId);
         try {
-            List<CoupleVO.CoupleList> coupleList = coupleService.getCoupleListPick();
+            List<CoupleVO.CoupleList> coupleList = coupleService.getCoupleList(params);
 
-            coupleList = coupleList.stream().map( obj-> {
+                // 반복 돌리지 않고 스트림으로 처리?
+                coupleList = coupleList.stream().map( obj-> {
                 String mbti = obj.getUserMbti();
                 String img = "/img/profileIcon/" + (obj.getUserGender().equals("남자") ? "m-"+mbti+".png" :"f-"+mbti+".png");
                 obj.setImages(img);
@@ -54,16 +67,19 @@ public class CoupleController {
         return view;
     }
 
+    // 필터링된 리스트 뿌리기 ajax 처리
     @PostMapping("/select")
     @ResponseBody
     public Map<String, Object> getCoupleListSelect(@RequestBody  Map<String, Object> requestData){
         Map<String, Object> resultMap = new HashMap<String, Object>();
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("userId",requestData.get("userID").toString());
         List<CoupleVO.CoupleList> coupleList = new ArrayList<CoupleVO.CoupleList>();
 
         try {
 
             List<String> selectedMBTI = (List<String>)requestData.get("selectedMBTI");
-            coupleList = coupleService.getCoupleListSelect(selectedMBTI);
+            coupleList = coupleService.getCoupleListSelect(selectedMBTI,param);
 
             coupleList = coupleList.stream().map( obj-> {
                 String mbti = obj.getUserMbti();
@@ -80,4 +96,74 @@ public class CoupleController {
         return resultMap;
     }
 
+    // 상세정보저장
+    @PostMapping("/saveDetails")
+    @ResponseBody
+    public Map<String, Object> saveDetail(@ModelAttribute CoupleVO.CoupleDetailInfo detailRequest){
+        Map<String, Object> resultMap = new HashMap<>();
+
+        try {
+                int result = coupleService.saveDetail(detailRequest);
+
+                if(result > 0){
+                    resultMap.put("resultCode",200);
+                }else{
+                    throw new Exception("save Error");
+                }
+
+        } catch (Exception e) {
+            resultMap.put("resultCode",500);
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
+
+
+    @PostMapping("/showDetailData")
+    @ResponseBody
+    public Map<String, Object> showDetailInfo(@RequestParam(value="thumbnailId") String thumbnailId){
+
+            Map<String, Object> resultMap = new HashMap<>();
+            List<CoupleVO.CoupleDetailInfo> detailInfo = new ArrayList<>();
+
+        try {
+            detailInfo = coupleService.showDetailInfo(thumbnailId);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resultMap.put("detailInfos",detailInfo);
+        return resultMap;
+    }
+
+    // 좋아요 목록에 추가
+    @PostMapping("/saveCoupleLikeList")
+    @ResponseBody
+    public Map<String, Object> saveLikeList(@RequestParam(value="userId", defaultValue = "test1") String userId,
+                                            @RequestParam(value="userTo", defaultValue = "test10") String userTo){
+
+
+        Map<String, Object> resultMap = new HashMap<>();
+        Map<String, Object> param = new HashMap<>();
+        param.put("userId", userId);
+        param.put("userTo", userTo);
+        try {
+            int result = coupleService.checkExistList(param);
+
+            // 0보다 크다면 디비에서 삭제 진행 쿼리 실행
+            if(result > 0){
+                result = coupleService.deleteLikeList(param);
+                resultMap.put("resultCode",201);
+            }else{
+                // 0 이면 디비에 저장 쿼리 실행
+                result = coupleService.addLikeList(param);
+                resultMap.put("resultCode",200);
+            }
+
+        } catch (Exception e) {
+            resultMap.put("resultCode",500);
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
 }
--- src/main/java/app/sum/bti/couple/vo/CoupleVO.java
+++ src/main/java/app/sum/bti/couple/vo/CoupleVO.java
@@ -1,9 +1,14 @@
 package app.sum.bti.couple.vo;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 public class CoupleVO {
+
 
     @Data
     public static class CoupleList{
@@ -11,12 +16,24 @@ public class CoupleVO {
         private String userId;
         private String userMbti;
         private String userGender;
+        private String userNick;
         private String loveMbti;
         private String hateMbti;
-        private String userNick;
-        private int likeCount; //  현재 리스트 인물을 조아요한 카운트
+        private String detailIntro;
+        private int likeCount; //  현재 리스트 인물을 좋아요한 카운트
         private String images;
+    }
+
+    @Data
+    public static class CoupleDetailInfo{
+
+        private String userId;
+        private String detailIntro;
+        private String detailHobby;
+        private String detailFood;
+        private String detailPat;
 
     }
+
 
 }
